add option to allow xaxis to have different time units for 1D time series plots and various other bug fixes

DP_diagnostics/DP_diagnostics_functions.py
@@ -12,7 +12,8 @@ import numpy as np
 import os
 
 def makevarlist(datadir,filelist,dimtest,varstoplot):
-    
+
+    print('Making list of variables to plot')
 #    numfiles=len(filelist)
     numfiles=1
     for f in range(0,numfiles):
@@ -40,10 +41,10 @@ def makevarlist(datadir,filelist,dimtest,varstoplot):
                 vartotest=fh.variables[varname][:]
                 if (vartotest.ndim == dimtest_rev):
                     theshape=np.shape(vartotest)
-                    print('STUFF ', varname, theshape[vartotest.ndim -1], ncol)
+#                    print('STUFF ', varname, theshape[vartotest.ndim -1], ncol)
                     if (theshape[vartotest.ndim - 1] == ncol):
                         varstoplot.append(varname)   
-                        print('APPEND ', varname)
+#                        print('APPEND ', varname)
                     
     return varstoplot
 

DP_diagnostics/DP_diagnostics_profiles.py
@@ -15,7 +15,7 @@ import DP_diagnostics_functions
 
 def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,caselist,derived_prof):
     
-    colorarr=["r","b","g","c","m","y"] # standard
+    colorarr=["r","b","g","c","m","y","k"] # standard
     
     numfiles=len(filelist)
     numtimes=len(avg_start)
@@ -35,7 +35,8 @@ def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,case
     numderived=len(derived_prof)
     for d in range(0,numderived):
         varstoplot.append(derived_prof[d])
-    
+
+    print('Now Plotting Profiles')
     ############################################################
     # loop over the variables to plot
     for x in range(0,len(varstoplot)):
@@ -63,8 +64,8 @@ def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,case
             varsinfile=fh.variables.keys()
                 
             # The True check is a temporary hack
-#            if (varname in varsinfile or True):
-            if (varname in varsinfile):
+            if (varname in varsinfile or True):
+#            if (varname in varsinfile):
             
                 # If derived variable search first for that
                 if (varname == "CLDLIQICE"):
@@ -140,7 +141,7 @@ def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,case
                     # Exceptions for plotting ease
                     if (theunits == "kg/kg"):
                         avgprof=avgprof*1000.
-                        theunits="g/kg"
+                        plottheunits="g/kg"
 
                     if (varname == "WQW_SEC"):
                         thelongname="SGS Moisture Flux"
@@ -154,6 +155,9 @@ def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,case
                     if (varname == "WTHL_RES"):
                         thelongname = "Resolved Heat Flux"
 
+                    # Copy units into a temporary arry in case manipulation needs to be done
+                    #   on these variables with similar units (like convert kg/kg to g/kg)
+                    plottheunits=theunits
                     # Only plot to top level
                     plotlevs=np.where(levarr > toplev)
 
@@ -164,7 +168,7 @@ def plotprofiles(datadir,plotdir,toplev,avg_start,avg_end,timelist,filelist,case
                     plt.title(thelongname + '\n' + \
                     '(' + varname + ')',fontsize=16)
                     plt.ylabel('P (hPa)',fontsize=14)
-                    plt.xlabel('('+theunits+')',fontsize=14)
+                    plt.xlabel('('+plottheunits+')',fontsize=14)
                     plt.grid(True)
                     plt.ticklabel_format(style='sci', axis='x', scilimits=(-4,4))
                     plt.xticks(fontsize=14)

DP_diagnostics/DP_diagnostics_time.py
@@ -13,7 +13,9 @@ import pylab
 import os
 import DP_diagnostics_functions
 
-def plot1Dtime(datadir,plotdir,time_start,time_end,filelist,caselist): 
+def plot1Dtime(datadir,plotdir,time_start,time_end,filelist,caselist,\
+               xaxis_opt="default",xaxis_units="time (days)",\
+               xaxis_mult=0,xaxis_start=0):
     
     colorarr=["r","b","g","c","m","y"]
     
@@ -65,6 +67,11 @@ def plot1Dtime(datadir,plotdir,time_start,time_end,filelist,caselist):
                     vartoplot2=np.squeeze(vartoplot[plottimes,:,:])
                 elif (vartoplot.ndim == 2):
                     vartoplot2=np.mean(vartoplot[plottimes,:],axis=1)
+
+                if (xaxis_opt == "custom"):
+                    time=(time*xaxis_mult)+xaxis_start
+                else:
+                    xaxis_units="time (days)"
             
                 plt.figure(x)
                 plt.plot(time[plottimes],vartoplot2,colorarr[f],linewidth=3)
@@ -73,7 +80,7 @@ def plot1Dtime(datadir,plotdir,time_start,time_end,filelist,caselist):
         
                 plt.title(fh.variables[varname].long_name + '\n' + \
                 '(' + varname + ')',fontsize=16)
-                plt.xlabel('time (days)',fontsize=14)
+                plt.xlabel(xaxis_units,fontsize=14)
                 if hasattr(fh.variables[varname],'units'):
                     plt.ylabel('('+fh.variables[varname].units+')',fontsize=14)
                 plt.grid(True)