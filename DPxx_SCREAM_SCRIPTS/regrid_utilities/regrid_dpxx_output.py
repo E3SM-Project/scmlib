6e9f47db8fc096a3bb66d4a59d40465e46a0d669
Peter Bogenschutz <bogenschutz1@llnlg.ov>
2024-09-21 17:41:03 -0700

    replace separate 2D and 3D regridders with one unified script

--- DPxx_SCREAM_SCRIPTS/regrid_utilities/regrid_dpxx_output.py
+++ DPxx_SCREAM_SCRIPTS/regrid_utilities/regrid_dpxx_output.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Process 3D fields and put them on an x & y grid.
-Data will be placed in a folder called post_processed_output/3D
+Process 2D & 3D fields and put them on an x & y grid.
+Data will be placed in a folder called post_processed_output
 in your case directory.
 
-Currently only works with data on the lev grid (as opposed to ilev).
+3D currently only works with data on the lev grid (as opposed to ilev).
 This is because ilev currently doesn't appear to be written to to
 output files for some reason.
 
@@ -19,21 +19,22 @@ import os
 import glob
 import matplotlib.pyplot as plt
 
-###### User input #####################################################
+###### Start user input ################################################
 # What variables do you want to process?  You have two options, you
-#   can either list selected variables or simply put "all" to regrid
-#   every 3D variable in your output stream. Example below of selected vars.
-#vartodo=["U","V"]
+#   can either list specific variables or simply put "all" to regrid every
+#   2D & 3D variable in your output stream. Example below of selected vars.
+#vartodo=["T_mid","IceWaterPath"]
 vartodo=["all"]
 
 # Supply the run directory, casename, and prefix of the output stream to process
 rundir='/pscratch/sd/b/bogensch/dp_screamxx/'
 casename='scream_dpxx_GATEIDEAL.3.2km.003a'
-outfilepre='scream_dpxx_GATEIDEAL.3.2km.003a.scream.15minute.3d.instant.INSTANT.nmins_x15'
+outstream='scream_dpxx_GATEIDEAL.3.2km.003a.scream.15minute.3d.instant.dyn.INSTANT.nmins_x15'
 
+###### End user input
 #######################################################################
 
-def arrange_4d_array(data, x_coords, y_coords):
+def arrange_array(data, x_coords, y_coords):
     # Create dictionaries to map the coordinates to their indices
     unique_x = sorted(set(x_coords))
     unique_y = sorted(set(y_coords))
@@ -44,27 +45,39 @@ def arrange_4d_array(data, x_coords, y_coords):
     # Determine the shape of the 2D array
     max_x = len(unique_x)
     max_y = len(unique_y)
-    
-    # Determine the number of slices (time or other dimension)
-    num_slices = data.shape[0]
-    num_levs = data.shape[2]  # Assuming the 3rd dimension is the number of levels
-    
-    # Create an empty 4D array with the determined shape
-    array_4d = np.empty((num_slices, num_levs, max_y, max_x))
-    
+
     # Convert x_coords and y_coords to indices
     x_indices = np.array([x_index[x] for x in x_coords])
     y_indices = np.array([y_index[y] for y in y_coords])
     
-    # Vectorized operation to place data into 4D array
-    # Use broadcasting to index array positions efficiently
-    array_4d[:, :, y_indices, x_indices] = data.transpose(0, 2, 1)
+    # Determine the number of slices (time or vertical levels)
+    num_slices = data.shape[0]
+    
+    if (data.ndim == 2):
+    
+       # Create an empty 3D array with the determined shape
+       arranged_array = np.empty((num_slices, max_y, max_x))
+    
+       # Use advanced indexing to place the data in the correct locations in one go
+       arranged_array[:, y_indices, x_indices] = data
+       
+    if (data.ndim == 3):
+       num_levs = data.shape[2]  # Assuming the 3rd dimension is the number of levels
+    
+       # Create an empty 4D array with the determined shape
+       arranged_array = np.empty((num_slices, num_levs, max_y, max_x))
+
+       # Vectorized operation to place data into 4D array
+       # Use broadcasting to index array positions efficiently
+       arranged_array[:, :, y_indices, x_indices] = data.transpose(0, 2, 1)
     
     # Create the appropriately arranged x and y coordinate arrays
     arranged_x_coords = np.array(unique_x)
     arranged_y_coords = np.array(unique_y)
     
-    return array_4d, arranged_x_coords, arranged_y_coords
+    return arranged_array, arranged_x_coords, arranged_y_coords
+
+################################
 
 def SCREAM_get_cords(initfile):
 
@@ -83,27 +96,67 @@ def SCREAM_get_cords(initfile):
    f.close()
 
    return time, lev, crm_grid_x, crm_grid_y
+
+################################
+
+def determine_var_dim(var,var_name,matching_vars,dimarr):
+
+   # Check if the dimensions are ('time', 'ncol')
+   if var.dimensions == ('time', 'ncol'):
+      matching_vars.append(var_name)
+      dimarr.append('2D')
    
-def find_3dvars_variables(filename):
+   # Check if the dimensions are ('time', 'ncol')
+   if var.dimensions == ('time', 'ncol','lev'):
+      matching_vars.append(var_name)
+      dimarr.append('3D')   
+
+################################
+   
+def find_variables(filename,varlist_in):
     # Open the NetCDF file
     dataset = nc4.Dataset(filename)
 
     # Initialize a list to store variable names that match the criteria
     matching_vars = []
+    dimarr = []
 
-    # Iterate over all the variables in the NetCDF file
-    for var_name in dataset.variables:
-        var = dataset.variables[var_name]
- 
-        # Check if the dimensions are ('time', 'ncol')
-        if var.dimensions == ('time', 'ncol','lev'):
-            matching_vars.append(var_name)
+    for var_entry in varlist_in:
+
+       # If all variables are done, search for variables in file
+       if (var_entry == "all"): 
+
+          # Iterate over all the variables in the NetCDF file
+          for var_name in dataset.variables:
+             var = dataset.variables[var_name]
+             determine_var_dim(var,var_name,matching_vars,dimarr)		
+       else:
+
+          # user specified output
+          var = dataset.variables[var_entry]
+          determine_var_dim(var,var_entry,matching_vars,dimarr)
 
     # Close the dataset
     dataset.close()
 
-    return matching_vars
+    return matching_vars, dimarr
 
+################################
+
+def check_path(dim):
+   
+   # Check to see if post process directory exists for 2d output; if not create it
+   postpath_dim=rundir+casename+'/post_processed_output/'+dim+'/'
+   ishere=os.path.isdir(postpath_dim)
+
+   # Make directory for post processing
+   if not ishere:
+      os.system('mkdir '+postpath_dim)
+      
+   return postpath_dim
+
+##############################################################################  
+##############################################################################
 ##############################################################################
 # Begin main function
 
@@ -114,20 +167,12 @@ ishere=os.path.isdir(postpath)
 # Make directory for post processing
 if not ishere:
    os.system('mkdir '+postpath)
-
-# Check to see if post process directory exists for 2d output; if not create it
-postpath_h2=rundir+casename+'/post_processed_output/3D/'
-ishere=os.path.isdir(postpath_h2)
-
-# Make directory for post processing
-if not ishere:
-   os.system('mkdir '+postpath_h2)
    
 ############################################################
 #
 
 # Make a list of files to process
-filedir=rundir+casename+'/run/'+outfilepre
+filedir=rundir+casename+'/run/'+outstream
 filelist=sorted(glob.glob(filedir+'*.nc'))
 print(filedir)
 # Get coordinates and timing information
@@ -155,8 +200,7 @@ if (numfiles > 1):
    ntimes=ntimes+len(time_in)
 
 # Are we doing all variables or selected variables?
-if (vartodo[0] == "all"):
-   vartodo=find_3dvars_variables(filelist[0])
+vartodo,dimarr=find_variables(filelist[0],vartodo)
 
 numvars=len(vartodo)
 
@@ -164,10 +208,11 @@ numvars=len(vartodo)
 # Process each variable one at a time
 for v in range(0,numvars):
 
-   fi=nc4.Dataset(filelist[0],mode='r')   
+   # Make sure output directory has been created
+   postpath_dim=check_path(dimarr[v])
 
-   print('Doing variable: ',vartodo[v])
-   outputfile=postpath_h2+casename+'_3D_'+vartodo[v]+'.nc'   
+   print('PROCESSING VARIABLE: ',vartodo[v])
+   outputfile=postpath_dim+casename+'_'+dimarr[v]+'_'+vartodo[v]+'.nc'   
    
    ishere=os.path.isfile(outputfile)
    print('Making output file ',outputfile)
@@ -177,14 +222,18 @@ for v in range(0,numvars):
    f.createDimension('x',len(arranged_x_coords))
    f.createDimension('y',len(arranged_y_coords))
    f.createDimension('time',ntimes)
-   f.createDimension('lev',len(lev_in))
+   if (dimarr[v] == '3D'):
+      f.createDimension('lev',len(lev_in))
 
    x=f.createVariable('x','f4','x')
    y=f.createVariable('y','f4','y')
-   lev=f.createVariable('lev','f4','lev')
    time=f.createVariable('time','f4','time')
-   out_var=f.createVariable(vartodo[v],'f4',('time','lev','y','x'))
-
+   if (dimarr[v] == '2D'):
+      out_var=f.createVariable(vartodo[v],'f4',('time','y','x'))
+   if (dimarr[v] == '3D'):
+      lev=f.createVariable('lev','f4','lev')
+      out_var=f.createVariable(vartodo[v],'f4',('time','lev','y','x'))
+      
    x[:]=arranged_x_coords
    x.units='m'
    x.long_name='x coordinate'
@@ -193,9 +242,10 @@ for v in range(0,numvars):
    y.units='m'
    y.long_name='y coordinate'
    
-   lev[:]=lev_in
-   lev.units='mb'
-   lev.long_name='hybrid level at midpoints'
+   if (dimarr[v] == '3D'):
+      lev[:]=lev_in
+      lev.units='mb'
+      lev.long_name='hybrid level at midpoints'      
 
    time.units='days'
    time.long_name='time'
@@ -212,12 +262,15 @@ for v in range(0,numvars):
       fi=nc4.Dataset(thefile,mode='r')
       time_in=fi.variables['time'][:]
       var=fi.variables[vartodo[v]][:]
-   
-      var_arranged,dummyx,dummyy=arrange_4d_array(var, crm_grid_x, crm_grid_y)
+
+      var_arranged,dummyx,dummyy=arrange_array(var, crm_grid_x, crm_grid_y)
    
       te=ts+len(time_in)
       time[ts:te]=time_in
-      out_var[ts:te,:,:,:]=var_arranged
+      if (dimarr[v] == '2D'):
+         out_var[ts:te,:,:]=var_arranged
+      if (dimarr[v] == '3D'):
+         out_var[ts:te,:,:,:]=var_arranged
       ts=te
    
       del(var)
